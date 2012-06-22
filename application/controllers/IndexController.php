193953228c23e9ec87272b3253a67db5649db560
kevin <kevin@otto>
2012-06-22 08:35:12 +0200

    Setting Up Zend With Testing

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -11,9 +11,6 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
-		phpinfo();
     }
-
-
 }
 
--- tests/application/controllers/IndexControllerTest.php
+++ tests/application/controllers/IndexControllerTest.php
@@ -1,14 +1,20 @@
 <?php
 
+require_once '../application/controllers/IndexController.php';
+
 class IndexControllerTest extends Zend_Test_PHPUnit_ControllerTestCase {
+
 
 	public function setUp() {
 		$this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
 		parent::setUp();
 	}
 
-	public function testInit() {
-		$this->assertTrue(true);
+	public function testIndexAction() {
+		$this->dispatch('/');
+		$this->assertController('index');
+		$this->assertAction('index');
+		$this->assertResponseCode(200);
 	}
 
 }
